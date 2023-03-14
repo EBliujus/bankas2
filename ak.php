e891bbb6e746707c18b4f6bd9a2e239915da5282
EBliujus <ev.bliujus@gmail.com>
2023-03-14 13:49:51 +0200

    saskaitos genervimas

--- ak.php
+++ ak.php
@@ -1,12 +1,13 @@
 <?php
 
-$I_sk = rand(6,7);
-$metu_sk = substr(rand(1910, 2023), -2);
+$I_sk = rand(5,6);
+$metu_sk = substr(rand(1923, 2023), -2);
 $menuo_sk = str_pad(rand(1, 12), 2, 0, STR_PAD_LEFT);
 $diena_sk = str_pad(rand(1, 31), 2, 0, STR_PAD_LEFT);
-$rand_sk = str_pad(rand(1, 999), 4, 0, STR_PAD_LEFT);
+$rand_sk = str_pad(rand(1, 999), 3, 0, STR_PAD_LEFT);
+$rand_sk2 = rand(1, 9);
 
-$ak = "{$I_sk}{$metu_sk}{$menuo_sk}{$diena_sk}{$rand_sk}";
+$ak = "{$I_sk}{$metu_sk}{$menuo_sk}{$diena_sk}{$rand_sk}{$rand_sk2}";
 
 echo $ak;
 ?>
